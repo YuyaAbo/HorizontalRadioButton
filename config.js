25813943c2a6f3419ad554defd504e1b4ac47052
YuyaAbo <b1011211@gmail.com>
2021-12-21 18:04:55 +0900

    Refactor

--- config.js
+++ config.js
@@ -1,47 +1,47 @@
 (function () {
-    const SELECTED_PARAMETER_NAME_KEY = 'selectedParameterNameKey'
-    let selectedParameterName = ''
+    tableau.extensions.initializeDialogAsync().then(() => {
+        $("#closeButton").click(closeDialog);
 
-    $(document).ready(function () {
-        tableau.extensions.initializeDialogAsync().then(function (openPayload) {
-            $('#closeButton').click(closeDialog)
+        const savedParameterName = tableau.extensions.settings.get(
+            "selectedParameterNameKey"
+        );
 
-            selectedParameterName = tableau.extensions.settings.get(SELECTED_PARAMETER_NAME_KEY)
-
-            tableau.extensions.dashboardContent.dashboard.getParametersAsync().then(function (parameters) {
+        tableau.extensions.dashboardContent.dashboard
+            .getParametersAsync()
+            .then(function (parameters) {
                 parameters.forEach(function (parameter) {
-                    if (parameter.allowableValues.type === tableau.ParameterValueType.List) {
-                        const listElement = $('<h3>')
+                    if (
+                        parameter.allowableValues.type === tableau.ParameterValueType.List
+                    ) {
+                        const listElement = $("<h3>");
 
-                        $('<input />', {
-                            type: 'radio',
+                        $("<input />", {
+                            type: "radio",
                             id: parameter.name,
-                            name: 'HorizontalRadioButtonConfig',
+                            name: "HorizontalRadioButtonConfig",
                             value: parameter.name,
-                            checked: parameter.name === selectedParameterName,
-                            click: function () { updateTargetParameter(parameter.name); },
-                        }).appendTo(listElement)
+                            checked: parameter.name === savedParameterName,
+                        }).appendTo(listElement);
 
-                        $('<label>', {
-                            'for': parameter.name,
+                        $("<label>", {
+                            for: parameter.name,
                             text: parameter.name,
-                        }).appendTo(listElement)
+                        }).appendTo(listElement);
 
-                        $('#parameters').append(listElement)
+                        $("#parameters").append(listElement);
                     }
-                })
-            })
-        })
-    })
+                });
+            });
+    });
 
-    function updateTargetParameter (parameterName) {
-        selectedParameterName = parameterName
-    }
-
-    function closeDialog () {
-        tableau.extensions.settings.set(SELECTED_PARAMETER_NAME_KEY, selectedParameterName)
+    const closeDialog = () => {
+        const selectedParameterName = $(`input:radio:checked`).val();
+        tableau.extensions.settings.set(
+            "selectedParameterNameKey",
+            selectedParameterName
+        );
         tableau.extensions.settings.saveAsync().then(() => {
-            tableau.extensions.ui.closeDialog(selectedParameterName)
-        })
-    }
-})()
+            tableau.extensions.ui.closeDialog("");
+        });
+    };
+})();
--- index.js
+++ index.js
@@ -1,66 +1,69 @@
 (function () {
-    let selectedParameterName = ''
-
-    $(document).ready(() => {
-        tableau.extensions.initializeAsync({'configure': configure}).then(() => {
-            tableau.extensions.settings.addEventListener(tableau.TableauEventType.SettingsChanged, onSettingsChange)
-            selectedParameterName = tableau.extensions.settings.get('selectedParameterNameKey')
-            initRadioButton()
-        })
-    })
-
-    const onSettingsChange =  (settingsEvent) => {
-        selectedParameterName = settingsEvent.newSettings.selectedParameterNameKey
-        initRadioButton()
-    }
+    const onSettingsChange = () => initRadioButton();
 
     const onParameterChange = (parameterChangeEvent) => {
         parameterChangeEvent.getParameterAsync().then((parameter) => {
-            $(`input:radio[value=${parameter.currentValue.formattedValue}]`)
-                .prop('checked', true)
-        })
-    }
+            $(`input:radio[value=${parameter.currentValue.formattedValue}]`).prop(
+                "checked",
+                true
+            );
+        });
+    };
 
     const initRadioButton = () => {
-        tableau.extensions.dashboardContent.dashboard.getParametersAsync().then((parameters) => {
-            const selectedParameter = parameters.find((p) => p.name === selectedParameterName)
-            selectedParameter.addEventListener(tableau.TableauEventType.ParameterChanged, onParameterChange)
+        const savedParameterName = tableau.extensions.settings.get(
+            "selectedParameterNameKey"
+        );
 
-            const parameterValuesElement = $('<div id="parameter">')
-            selectedParameter.allowableValues.allowableValues.forEach((dataValue) => {
-                const eachValueElement = $('<div style="display: inline-block">')
+        tableau.extensions.dashboardContent.dashboard
+            .getParametersAsync()
+            .then((parameters) => {
+                const selectedParameter = parameters.find(
+                    (p) => p.name === savedParameterName
+                );
+                selectedParameter.addEventListener(
+                    tableau.TableauEventType.ParameterChanged,
+                    onParameterChange
+                );
 
-                $('<input />', {
-                    type: 'radio',
-                    id: dataValue.formattedValue,
-                    name: selectedParameter.name,
-                    value: dataValue.formattedValue,
-                    checked: dataValue.formattedValue === selectedParameter.currentValue.formattedValue,
-                    click: () => selectedParameter.changeValueAsync(dataValue.formattedValue),
-                }).appendTo(eachValueElement)
+                const parameterValuesElement = $('<div id="parameter">');
+                selectedParameter.allowableValues.allowableValues.forEach(
+                    (dataValue) => {
+                        const eachValueElement = $('<div style="display: inline-block">');
 
-                $('<label>', {
-                    'for': dataValue.formattedValue,
-                    text: dataValue.formattedValue,
-                }).appendTo(eachValueElement)
+                        $("<input />", {
+                            type: "radio",
+                            id: dataValue.formattedValue,
+                            name: selectedParameter.name,
+                            value: dataValue.formattedValue,
+                            checked:
+                                dataValue.formattedValue ===
+                                selectedParameter.currentValue.formattedValue,
+                            click: () =>
+                                selectedParameter.changeValueAsync(dataValue.formattedValue),
+                        }).appendTo(eachValueElement);
 
-                parameterValuesElement.append(eachValueElement)
-            })
-            $('#parameter').replaceWith(parameterValuesElement)
-        })
-    }
+                        $("<label>", {
+                            for: dataValue.formattedValue,
+                            text: dataValue.formattedValue,
+                        }).appendTo(eachValueElement);
 
-    function configure () {
-        tableau.extensions.ui.displayDialogAsync('./config.html', selectedParameterName).then((closePayload) => {
+                        parameterValuesElement.append(eachValueElement);
+                    }
+                );
+                $("#parameter").replaceWith(parameterValuesElement);
+            });
+    };
 
-        }).catch((error) => {
-            switch (error.errorCode) {
-                case tableau.ErrorCodes.DialogClosedByUser:
-                    console.log('Dialog was closed by user');
-                    break;
-                default:
-                    console.error(error.message);
-            }
-        });
-    }
+    const configure = () => {
+        tableau.extensions.ui.displayDialogAsync("./config.html", "");
+    };
+
+    tableau.extensions.initializeAsync({ configure: configure }).then(() => {
+        tableau.extensions.settings.addEventListener(
+            tableau.TableauEventType.SettingsChanged,
+            onSettingsChange
+        );
+        initRadioButton();
+    });
 })();
